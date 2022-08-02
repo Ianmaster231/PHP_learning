ecf7f35ae02a4077095232041502b014754248a4
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-08-02 12:19:15 +0200

    first commit

---
+++ variables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    $name = "Ian";
+    $number = 100;
+
+    echo $name . " " . $number;
+
+    $name = "<h1>Hello</h1>";
+    echo $name;
+    // . means concatination (Same function as +)
+
+
+    //Best format to write variabes
+    //Best Practice Example: --> $numberList <--
+    //so all lower case then if second word 
+    //Have large first letter of it
+    ?>
+</body>
+</html>
