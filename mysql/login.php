b55cb5782c162fc6e9ca5a276d4205e3616c7786
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-08-10 18:04:08 +0200

    Started phmyadmin section did 5 videos today

---
+++ mysql/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="col-xs-6">
+            <form action="login.php" method="post">
+                <div class="form-roup">
+                    <label for="username" class="username">Username</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-roup">
+                    <label for="password" class="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <br>
+                <input type="submit" class="btn btn-primary" name="Submit" value="Submit">
+            </form>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
