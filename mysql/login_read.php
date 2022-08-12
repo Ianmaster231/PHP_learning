f94eb2eac87a431f89c79d2d864be5ef4b0a6cb8
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-08-12 10:20:35 +0200

    before lesson 48 is done and removes the update fucntion

---
+++ mysql/login_read.php
@@ -0,0 +1,52 @@
+<?php 
+        $connection = mysqli_connect('localhost','root','','loginapp');
+
+            if($connection){
+                echo "We ae connected";
+            } else {
+                die("Database connection failed");
+            }
+
+    $query = "SELECT * FROM users";
+   $result = mysqli_query($connection,$query);
+
+   if(!$result){
+    die('Query failed' );
+   }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="col-sm-6">
+            <?php 
+            //gives more detail in the array for mysqli_fetch_assoc
+                while($row = mysqli_fetch_assoc($result)) {
+
+                    ?>
+                    <pre>
+                        <?php
+                        print_r($row);
+                        ?>
+                    </pre>
+                    <?php
+
+                    
+                }
+           ?>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
